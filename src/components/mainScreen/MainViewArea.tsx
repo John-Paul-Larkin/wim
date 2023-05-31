1282e99979fdc19ae7d55d2de55a2ac0e074e575
john Paul Larkin <johnplarkin@gmail.com>
2023-05-31 11:21:29 +0100

    refactor

---
+++ src/components/mainScreen/MainViewArea.tsx
@@ -0,0 +1,24 @@
+import { Dashboard } from "../Dashboard";
+import { PurchaseOrders } from "../PurchaseOrders";
+import { Customers } from "../customer/Customers";
+import { Employees } from "../employees/Employees";
+import { Products } from "../products/Products";
+import { Salesorders } from "../salesOrders/Salesorders";
+import { Suppliers } from "../suppliers/Suppliers";
+import "./MainViewArea.css";
+
+export const MainViewArea = ({ selectedNavComponent }: { selectedNavComponent: string }) => {
+  
+  //returns the selected component
+  return (
+    <div className="section-view-area">
+      {selectedNavComponent === "Dashboard" && <Dashboard />}
+      {selectedNavComponent === "Products" && <Products />}
+      {selectedNavComponent === "Sales Orders" && <Salesorders />}
+      {selectedNavComponent === "Purchase Orders" && <PurchaseOrders />}
+      {selectedNavComponent === "Customers" && <Customers />}
+      {selectedNavComponent === "Suppliers" && <Suppliers />}
+      {selectedNavComponent === "Employees" && <Employees />}
+    </div>
+  );
+};
