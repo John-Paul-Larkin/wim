d5c2b20c84ec296838811195c7ff93eff4e19f75
john Paul Larkin <johnplarkin@gmail.com>
2023-07-19 20:37:09 +0100

    rebuild test

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import "regenerator-runtime";
 import App from "./App.js";
 import "./index.css";
 
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
       <App />
