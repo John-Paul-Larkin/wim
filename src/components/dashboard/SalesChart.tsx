db372d82970a71bce7c942cb232cc855d30fd00e
john Paul Larkin <johnplarkin@gmail.com>
2023-07-23 15:23:16 +0100

    git pat test

--- src/components/dashboard/SalesChart.tsx
+++ src/components/dashboard/SalesChart.tsx
@@ -96,6 +96,7 @@ export default function SalesChart() {
           //     // color: "white",
           //     backgroundColor: "blue",
           //   }),
+          //
           // }}
         ></Select>
       </div>
